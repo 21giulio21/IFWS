6e7505cc5a0ae1fcffd05eae79e57d757fd16e52
21giulio21 <21giulio21@gmail.com>
2018-06-05 16:41:29 +0200

    g

--- InstagramGetFollows/InstagramAPI.py
+++ InstagramGetFollows/InstagramAPI.py
@@ -1,10 +1,4 @@
-'''
-Questo main permette di dare in input un nome ad esempio fedez
-e scarico tutti gli id e i nomi delle persone che lo seguono
-cosi ho la certezza che sono tutte persone vere e non capitano mai
-nomi del tipo: 001 ecc..
-'''
-
+import base64
 import requests
 import re
 import time
@@ -30,25 +24,11 @@ def follow(id,username, cookies, csrf):
 
 	}
 
-
-
 	response = requests.post('https://www.instagram.com/web/friendships/' + str(id) + '/follow/', 	headers=headers)
-	print(response.content)
+	print("FOLLOW" + username + response.content)
 
-#NB. Original query string below. It seems impossible to parse and
-#reproduce query strings 100% accurately so the one below is given
-#in case the reproduced version is not "correct".
-# response = requests.post('https://www.instagram.com/web/friendships/232257039/follow/?hl=en', headers=headers)
-
-
-
-
-
-
-'''
-DA SISTEMARE
-'''
-def unfollow():
+#TODO: Da ultimare
+def unfollow(id,username, cookies, csrf):
 
     headers = {
         'origin': 'https://www.instagram.com',
@@ -56,17 +36,18 @@ def unfollow():
         'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7',
         'x-requested-with': 'XMLHttpRequest',
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
-        'cookie': 'csrftoken=lFhhLQoVs9oV50indCYzEq7IcgqDtWYe; shbid=18815; ds_user_id=819693525; rur=FRC; mid=WvmbbgAEAAGkE8-Iw_fd2dUvCtgG; sessionid=IGSC9796ca437fcfd6d36398ab15e9a206dec873970556b0fe021dc95ff271b319f1%3ATv29t528QoF7ZhOLnfv3eghfYFZwWSRt%3A%7B%22_auth_user_id%22%3A819693525%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A4%2C%22_token_ver%22%3A2%2C%22_token%22%3A%22819693525%3At78X57JbUvMQlDmPz5isBb5F4xzbvugf%3A20cdd64da7cfe4973813334c7d0de224fab20e6de545dcecd280dc280e6daa35%22%2C%22last_refreshed%22%3A1526307694.726708889%7D; mcd=3; fbm_124024574287414=base_domain=.instagram.com; fbsr_124024574287414=0_t2vXeIk8q8fy5LPXxymigkKjiDRJ3NzA3P_RwvM60.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUURBZ1NxeElsU205a05RX2kwU04wUEZEaTJENEFnZkF2ZzBpeWd0SEQybC15NVF4Q2lIcVY5ZVJsN3IwbFdkRGUzclZyWDA2TmJNMlE1M2NpMGdfNTU2ZV9mMEhFZXhzdENaT2Jxd29BZ256OEVzU3lBV2Nsb2NyR0c4ODRmeWF6b21oVF9WUXhRTWstOE9VQlhKc2hUT2ZJOW4zeHd0OEgzUy1acnlmTHZsbHVlaUVRSmI0Y0xHUERrbTYya01xdlNmcC1vdnRjLU4xbmNseEVOMVZmb1AycXh1SE9wbWgtUWF2SkZGMXhCVXl2OFE3S0N5TzN3X3IwcnNJQkw3MFc5OGZERVk4b0pzb0lDaUZfRFFEblBDQ3ZQd3pVUXZ1ZlNpYXJGOG1NNzBDV3Y4TFhSVjI0c1l1RG9XUVhpQW4wN0x0Uy04eGFNQ2Z1WjBxcVhlQWhZZSIsImlzc3VlZF9hdCI6MTUyNjMwODMyNiwidXNlcl9pZCI6IjExNTQwMjExNjMifQ; shbts=1526308327.404692; urlgen="{\\"time\\": 1526307694\\054 \\"193.55.113.196\\": 2200}:1fIEWF:VLTfQwtdAehiKHQaTUTc3rPUepA"',
-        'x-csrftoken': 'lFhhLQoVs9oV50indCYzEq7IcgqDtWYe',
+        'cookie': cookies,
+        'x-csrftoken': csrf,
         'x-instagram-ajax': '0fa00dc2cc1f',
         'content-type': 'application/x-www-form-urlencoded',
         'accept': '*/*',
-        'referer': 'https://www.instagram.com/fedez/',
+        'referer': 'https://www.instagram.com/'+username+'/',
         'authority': 'www.instagram.com',
         'content-length': '0',
     }
 
-    response = requests.post('https://www.instagram.com/web/friendships/232257039/unfollow/', headers=headers)
+    response = requests.post('https://www.instagram.com/web/friendships/'+id+'/unfollow/', headers=headers)
+    print("UNFOLLOW" + username + response.content)
 
 
 def login(username,password):
@@ -101,6 +82,31 @@ def login(username,password):
 
 def getUsersToFollow():
     return json.loads(requests.get(url_get_all_user).content)
+
+#Agggiorno l'array sul database
+def updateUserFollowed(userFollowed,username):
+    url = "http://getfollowersoninstagram.altervista.org/updateUserFollowed.php?username="+username+"&users_followed="+userFollowed
+    requests.get(url)
+
+#Salvo id dell'utente nel database
+def saveIdIntoDatabase(username,id):
+    url = "http://getfollowersoninstagram.altervista.org/saveIdIntoDatabase.php?username="+username+"&id="+id
+    requests.get(url)
+
+#salvo i cookie di un relativo utente sul server
+def seveCookieIntoServer(username,cookie):
+    cookie =  base64.b64encode(str(cookie))
+    url = "http://getfollowersoninstagram.altervista.org/saveCookie.php?username=" + str(username) +"&cookie="+str(cookie)
+    print requests.get(url).content
+
+
+
+
+#prendo come input un numero random da 1 al numero massimo di persone che ho nel database di persone che posso
+#seguire e facci ola richiesta pert farmene tornare 1 a caso
+def getRandomUserToFollow(index):
+    url = "http://getfollowersoninstagram.altervista.org/getUserToFollowFromIndex.php?index=" + str(index)
+    return json.loads(requests.get(url).content)
 
 #Ritorna quanti siano gli utenti registrati
 def countUserIntoDatabase():
--- InstagramGetFollows/script.py
+++ InstagramGetFollows/script.py
@@ -1,9 +1,19 @@
+import base64
 import time
+import ast
+from InstagramAPI import updateUserFollowed
+from InstagramAPI import saveIdIntoDatabase
+from InstagramAPI import seveCookieIntoServer
+from InstagramAPI import follow
+from InstagramAPI import login
+from InstagramAPI import unfollow
+from InstagramAPI import getRandomUserToFollow
 from InstagramAPI import countUserIntoDatabase
 from InstagramAPI import selectUserFromDatabase
 from InstagramAPI import getIDFromUsername
 from InstagramAPI import getCountUsersToFollow
 from random import randint
+import re
 
 
 
@@ -31,19 +41,36 @@ while True:
         username = str(user[user.find("u'USERNAME': u'")+len("u'USERNAME': u'"):user.find("', u'COOKIES'")])
         cookie = user[user.find("u'COOKIES': u'")+len("u'COOKIES': u'"):user.find("', u'PASSWORD_INSTAGRAM'")]
         follow_unfollow = user[user.find("u'FOLLOW_UNFOLLOW': u'")+len("u'FOLLOW_UNFOLLOW': u'"):user.find("'}]")]
-        users_followed = user[user.find("u'USERS_FOLLOWED': u'")+len("u'USERS_FOLLOWED': u'"):user.find("', u'SCRIPT_ACTIVE'")]
-        print(users_followed)
-
+        users_followed_array = re.split(';', user[user.find("u'USERS_FOLLOWED': u'")+len("u'USERS_FOLLOWED': u'"):user.find("', u'SCRIPT_ACTIVE'")])
+        users_followed_string =  user[user.find("u'USERS_FOLLOWED': u'")+len("u'USERS_FOLLOWED': u'"):user.find("', u'SCRIPT_ACTIVE'")]
 
         #Controllo che siano settati i cookie dell'utente altrimenti li chiedo a instagram
         #facendo il login
         if len(cookie) == 0:
-            print("cookie dell utente nullo")
-            #TODO: cookie = ...
+            username = "ginotani82324"
+            password = "21giulio21"
+            r = login(username, password)
+            cookies_dict = r.cookies.get_dict()
+
+            #Salvo la variabile cookies_dict sul server
+            seveCookieIntoServer(username,cookies_dict)
+        else:
+            cookies_dict = ast.literal_eval(base64.b64decode(str(cookie)))
+
+        #Sia se ho ottenuto i cookie da instagram o dal mio server setto bene la variabile cookies_str
+        cookies_str = ''.join(key + "=" + str(cookies_dict[key]) + "; " for key in cookies_dict)
 
         if len(id) == 0:
-            print("ID dell utente nullo")
             id = getIDFromUsername(username)
+            saveIdIntoDatabase(username, id)
+
+
+        #controllo che sono al massimo di persone che posso seguire al giorno
+        if len(users_followed_array) > 0: #max_requests:
+
+            #Se sono al numero di persone massime imposto users_followed a 0
+            # In questo modo inizio a fare richieste di unfollow
+            follow_unfollow = 0
 
         #Se follow_unfollow e' 1 allora devo seguire una persona a caso tra tutte quelle  nel database
         if follow_unfollow == "1":
@@ -52,13 +79,32 @@ while True:
             count_user_to_follow = getCountUsersToFollow()
 
             #Seleziono 1 utente tra 0 e count_user_to_follow da seguire
-            random_user_to_follow = str(randint(1, int(count_user_to_follow)))
+            random_number_user_to_follow = str(randint(1, int(count_user_to_follow)))
+            user_to_follow = str(getRandomUserToFollow(random_number_user_to_follow))
+            username_user_to_follow = user_to_follow[user_to_follow.find("u'USERNAME': u'") + len("u'USERNAME': u'"): user_to_follow.find("', u'ID':")]
+            id_user_to_follow = user_to_follow[user_to_follow.find("', u'ID': u'") + len("', u'ID': u'"): user_to_follow.find("'}")]
 
-            print(random_user_to_follow)
+            #Seguo la persona che ho scaricato
+            follow(id_user_to_follow,username_user_to_follow,cookies_str,cookies_dict['csrftoken'])
+
+            #Devo aggiundere l'utente alla stringa totale delle persone seguite
+            users_followed_string = users_followed_string + ";" + username_user_to_follow
+            updateUserFollowed(users_followed_string,username)
+
+        else:
+
+
+#TODO DA FARE!!
+            # Devo fare richieste di UNFOLLOW
+            username_to_unfollow = users_followed_array[0]
+            id_to_unfollow = getIDFromUsername(username_to_unfollow)
+            unfollow(id_to_unfollow,username_to_unfollow,cookies_str,cookies_dict['csrftoken'])
+            users_followed_array.remove(users_followed_array[0])
+            users_followed_string = ''.join(item for item in users_followed_array)
+            updateUserFollowed(users_followed_string,username)
 
 
 
 
 
 
-        #Se il campo ID e' nullo allora chiedo l'id dell'utente a instagram
