3ab656881369801491f9f128212233d9bb647eb2
Giulio Tavella <giulio@MacBook-Air-di-laura.local>
2018-08-27 14:25:58 +0200

    g

--- script/scriptScript2Thread.sh
+++ script/scriptScript2Thread.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#Il primo parametro indica quanto tempo aspettare dopo averli finiti, il secondo che gruppo prendere di utenti da processare
+#Il primo parametro indica quanto tempo aspettare dopo averli finiti, il secondo che gruppo prendere di utenti da processare e il terzo indica dopo quato fare unfollow
 while [ true ] ; do
 
-python ../InstagramGetFollows/script2Thread.py $1 $2
+python ../InstagramGetFollows/script2Thread.py $1 $2 $3
 
 done
 
