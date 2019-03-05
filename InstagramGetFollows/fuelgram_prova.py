08669ea8b332c517dc548339e00d962fc446209d
Tavella <giulio.tavella@accenture.com>
2019-03-05 17:23:10 +0100

    d

--- InstagramGetFollows/fuelgram_prova.py
+++ InstagramGetFollows/fuelgram_prova.py
@@ -1,4 +1,4 @@
-#import instaloader
+import instaloader
 import json
 import time
 import datetime as dt
@@ -97,10 +97,8 @@ headers = {
 
 
 def configurazioneSLOT(username_recive_likes,username_get_likes,ID_SLOT):
-    IG_id_recive_likes = getIDFromUsername_PY3(username_recive_likes)
-    IG_id_get_likes = getIDFromUsername_PY3(username_get_likes)
-
-    print(IG_id_recive_likes , IG_id_get_likes)
+    IG_id_recive_likes = instaloader.Profile.from_username(L.context, username_recive_likes).userid
+    IG_id_get_likes = instaloader.Profile.from_username(L.context, username_get_likes).userid
 
 
 
@@ -151,29 +149,15 @@ def getRount():
         print("BHO")
         return '["10:30"]'
 
-def getIDFromUsername_PY3(username):
 
-    headers = {
-        'authority': 'www.instagram.com',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.181 Chrome/66.0.3359.181 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cookie': 'csrftoken=tpNpx90YcinKiWlaLcx3apvueW0OpZV9; shbid=18815; rur=FRC; mid=Ww6TeAAEAAHCATvZQX6W_Jih5thX; ds_user_id=819693525; sessionid=IGSC8ed527fc1cda43ac5555695cbba25d643a1f566c1a145452aeb5b67b12fb5305%3A17hUaA9Ul0DdZyAsj2Os4HkJ1yVzZfCg%3A%7B%22_auth_user_id%22%3A819693525%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A4%2C%22_token_ver%22%3A2%2C%22_token%22%3A%22819693525%3Ap0rX7NmINkKdhHbEOdYn6Ku6bS6zoapm%3Ae88d7822ccb18324c4369523a052ca1680c61add19ecc6513c6466483123a6c0%22%2C%22last_refreshed%22%3A1527681913.5427627563%7D; ig_cb=1; mcd=3; urlgen="{\\"time\\": 1527681913\\054 \\"193.55.113.196\\": 2200}:1fO1o7:2az6OzqMKD6FoWtZ4xZOuq8St1Q"',
-    }
-
-    response = str(requests.get('https://www.instagram.com/' + username, headers=headers).content)
-    posizioneprofilePage_= response.find("profilePage_")
-    inizio_id = response[posizioneprofilePage_ + len("profilePage_"):]
-    id = str(inizio_id[:inizio_id.find("\"")])
-    return id
 
 # Queste credenziali sono di un profilo a caso che permette solamente di
     # avere accesso ai post e dati dei p
 username_IG = "instatrack.eu"
 password_IG = "vikagiulio2121"
 
+L = instaloader.Instaloader()
+L.login(user=username_IG, passwd=password_IG)
 
 
 
